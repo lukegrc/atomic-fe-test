d00fac64f8f92ae809dba11cdc4944f405435cb1
Luke Grech <lukegrech2002@gmail.com>
2025-09-03 09:09:06 +0200

    feat: add basic NestJS app structure

---
+++ backend/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { MoviesModule } from './movies/movies.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HttpModule,
+    MoviesModule,
+  ],
+})
+export class AppModule {}
---
+++ backend/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS for frontend communication
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  await app.listen(3001);
+  console.log('Backend server running on http://localhost:3001');
+}
+bootstrap();
